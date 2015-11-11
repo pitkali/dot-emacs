ac7c9fd3157583e3df9c9540451e02c92855c29a
Piotr Kalinowski <pkalinowski@opera.com>
2015-11-11 16:44:45 +0100

    ELPA update

---
+++ elpa/isearch+-20151026.1026/isearch+-pkg.el
@@ -0,0 +1,1 @@
+(define-package "isearch+" "20151026.1026" "Extensions to `isearch.el' (incremental search)." 'nil :url "http://www.emacswiki.org/isearch+.el" :keywords '("help" "matching" "internal" "local"))
