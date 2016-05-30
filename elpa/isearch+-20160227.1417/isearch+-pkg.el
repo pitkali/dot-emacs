6cb39b0543c953428d0758d3bb0aac830b2f638d
Piotr Kalinowski <pitkali@gmail.com>
2016-05-31 01:47:37 +0200

    ELPA update

---
+++ elpa/isearch+-20160227.1417/isearch+-pkg.el
@@ -0,0 +1,1 @@
+(define-package "isearch+" "20160227.1417" "Extensions to `isearch.el' (incremental search)." 'nil :url "http://www.emacswiki.org/isearch+.el" :keywords '("help" "matching" "internal" "local"))
