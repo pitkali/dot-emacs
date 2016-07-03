5869eda81c5766f618d38245d798b31d64cb3a78
Piotr Kalinowski <pitkali@gmail.com>
2016-07-04 00:08:06 +0200

    ELPA update

--- elpa/pcache-20151109.639/pcache-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; pcache-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("pcache.el") (22083 24855 163561 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; pcache-autoloads.el ends here
