285a6ccd906a1bcfb49c994264ab8fc9a11ecb96
Piotr Kalinowski <pkalinowski@opera.com>
2016-12-14 15:55:23 +0100

    ELPA update

--- elpa/isearch+-20161205.1445/isearch+-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "isearch+" "20161205.1445" "Extensions to `isearch.el' (incremental search)." 'nil :url "http://www.emacswiki.org/isearch+.el" :keywords '("help" "matching" "internal" "local"))
